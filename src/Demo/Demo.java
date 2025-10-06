22a19db44329d6fd2b3e17af7265d7ce30b025a9
Shivaji Kalyankar <shivajikalyankar25@gmail.com>
2025-10-06 15:25:40 +0530

    First code Push

---
+++ src/Demo/Demo.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hell wecome to Sharvil's System Worls");
+		System.out.println("THis is my FIrst java Program");
+		}
+
+}
---
+++ src/Demo/Lab_02_Variables.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Lab_02_Variables {
+
+	public static void main(String[] args) {
+	System.out.println("Hello Word");
+		
+	}
+
+}
+
